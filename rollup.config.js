50bcf776d8a6a622398228b09a940d6161d7c215
Idris Khenchil <idriskhenchil@gmail.com>
2022-12-19 14:32:58 -0500

    Fixed naming

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,14 @@
-// The banner to add to the top of each file
-// Pulls details from the package.json file
 let banner = `// Copyright (c) Idris Khenchil https://github.com/idriskhenchil https://github.com/idriskhenchil/Fort.js`
 
-// The formats to output
-// Full list here: https://rollupjs.org/guide/en/#outputformat
-let formats = ['iife', 'es', 'cjs'];
+let formats = ['es', 'cjs', 'umd'];
 
-// The files to compile with rollup.js,
 export default formats.map(function (format) {
     return {
         input: 'src/fort.js',
         output: {
             file: `dist/fort.${format}.js`,
             format: format,
-            name: 'Fort.js',
+            name: 'Fort',
             banner: banner,
             exports: 'auto'
         }
